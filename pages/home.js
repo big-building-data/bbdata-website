e442dbc767738f955aa80ee9c957bd3082581ca8
Fred Montet <fredmontet@gmail.com>
2020-05-05 11:13:34 +0200

    Fixed URLs

--- pages/home.js
+++ pages/home.js
@@ -2,28 +2,28 @@ export const home = {
     hero: {
         title: 'BBData',
         text: 'The scalable platform for all your sensors and time series data, for the building sector and more',
-        image_src: "../../static/home/buildings2_wh.png",
+        image_src: "home/buildings2_wh.png",
         image_alt: "buildings"
     },
 
     firstFeature: {
         title: 'Easy and Secure',
         text: 'As a user, you only see what your administator let you access. Furthermore, our servers are located in Switzerland and all their connections are encrypted.',
-        image_src: "../../static/home/SitReadingDoodle.png",
+        image_src: "home/SitReadingDoodle.png",
         image_alt: ""
     },
 
     secondFeature: {
         title: 'Scalable Storage',
         text: 'If you want to store the data from a single sensor or more that thousand, no problem. We use the latest big data platform to back your data storage need.',
-        image_src: "../../static/home/MessyDoodle.png",
+        image_src: "home/MessyDoodle.png",
         image_alt: ""
     },
 
     thirdFeature: {
         title: 'Generic Use Cases',
         text: 'You have other plans outside from the building use case? BBData is a generic tool that can fit industrial actors, researchers, hobbyist and more. ',
-        image_src: "../../static/home/GroovySittingDoodle.png",
+        image_src: "home/GroovySittingDoodle.png",
         image_alt: ""
     },
 
