0619a7c5b55a89c2329876b6fb5628bf76a4d5f0
Fred Montet <fredmontet@gmail.com>
2020-05-05 11:10:40 +0200

    useBaseUrl trial

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ function Home() {
                 <HomeBanner
                     title={hero.title}
                     text={hero.text}
-                    image_src={hero.image_src}
+                    image_src={useBaseUrl(hero.image_src)}
                     image_alt={hero.image_alt}/>
             </header>
             <main>
@@ -38,21 +38,21 @@ function Home() {
                         <FeatureLeft
                             title={firstFeature.title}
                             text={firstFeature.text}
-                            image_src={firstFeature.image_src}
+                            image_src={useBaseUrl(firstFeature.image_src)}
                             image_alt={firstFeature.image_alt}/>
                     </Row>
                     <Row className={styles.Row}>
                         <FeatureRight
                             title={secondFeature.title}
                             text={secondFeature.text}
-                            image_src={secondFeature.image_src}
+                            image_src={useBaseUrl(secondFeature.image_src)}
                             image_alt={secondFeature.image_alt}/>
                     </Row>
                     <Row className={styles.Row}>
                         <FeatureLeft
                             title={thirdFeature.title}
                             text={thirdFeature.text}
-                            image_src={thirdFeature.image_src}
+                            image_src={useBaseUrl(thirdFeature.image_src)}
                             image_alt={thirdFeature.image_alt}/>
                     </Row>
                     <ScrollableAnchor id={'Contact'}>
