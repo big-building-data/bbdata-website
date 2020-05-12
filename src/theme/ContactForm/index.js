80c7943456461a550f8a941df5efe8f637f34af8
Claudio Herren <claudio.herren@edu.hefr.ch>
2020-05-12 16:05:19 +0200

    Form validation added
    Validating Form and showing error messages.

--- src/theme/ContactForm/index.js
+++ src/theme/ContactForm/index.js
@@ -1,47 +1,79 @@
 import React from "react";
+import { useForm, ErrorMessage } from "react-hook-form";
+
 import styles from "./styles.module.css";
 
 import Buttons from '../Button/index';
 
 import {Col, Form, FormControl, FormLabel, FormText} from "react-bootstrap";
 
-const ContactForm = (props) => (
-    <div>
-        <Form>
-            <Form.Row className={styles.FormRow}>
-                <Col lg={1}>
-                    <FormLabel column={0}>{props.name}</FormLabel>
-                </Col>
-                <Col lg={11}>
-                    <FormControl type="text" placeholder="Enter Name" required/>
-                </Col>
-            </Form.Row>
+function ContactForm(props) {
+    const { register, handleSubmit, errors } = useForm({validateCriteriaMode: "all"});
 
-            <Form.Row className={styles.FormRow}>
-                <Col lg={1}>
-                    <FormLabel column={0}>{props.email}</FormLabel>
-                </Col>
-                <Col lg={11}>
-                    <FormControl type="email" placeholder="Enter E-Mail address" required/>
-                    <FormText>{props.text}</FormText>
-                </Col>
-            </Form.Row>
+    function onSubmit(data) {
+        console.log(data);
+    }
 
-            <Form.Row className={styles.FormRow}>
-                <Col lg={1}>
-                    <FormLabel column={0}>{props.message}</FormLabel>
-                </Col>
-                <Col lg={11}>
-                    <FormControl as="textarea" rows="5" required/>
-                </Col>
-            </Form.Row>
+    return (
+        <div>
+            <Form onSubmit={handleSubmit(onSubmit)} name="Contact" method="POST" data-netlify="true">
+                <Form.Row className={styles.FormRow}>
+                    <Col lg={1}>
+                        <FormLabel column={0}>{props.name}</FormLabel>
+                    </Col>
+                    <Col lg={11}>
+                        <FormControl type="text" placeholder="Enter Name" name="Name" ref={register({required: "Input required"})} className={styles.Input} />
+                        <ErrorMessage name="Name" errors={errors} />
+                    </Col>
+                </Form.Row>
 
-            <Form.Row className={styles.Button}>
-                <Buttons type="SubmitBtn"/>
-            </Form.Row>
+                <Form.Row className={styles.FormRow}>
+                    <Col lg={1}>
+                        <FormLabel column={0}>{props.email}</FormLabel>
+                    </Col>
+                    <Col lg={11}>
+                        <FormControl
+                            type="email"
+                            placeholder="Enter E-Mail address"
+                            name="Email"
+                            ref={register({
+                                required: "Input required",
+                                pattern: {
+                                    value: /^\S+@\S+$/i,
+                                    message: "Invalid Email address",
+                                },
+                            })}
+                            className={styles.Input}
+                        />
+                        <ErrorMessage name="Email" errors={errors} >
+                            {({ messages }) =>
+                            messages &&
+                                Object.entries(messages).map(([type, message]) => (
+                                    <p key={type}>{message}</p>
+                                ))
+                            }
+                        </ErrorMessage>
+                        <FormText>{props.text}</FormText>
+                    </Col>
+                </Form.Row>
 
-        </Form>
-    </div>
-);
+                <Form.Row className={styles.FormRow}>
+                    <Col lg={1}>
+                        <FormLabel column={0}>{props.message}</FormLabel>
+                    </Col>
+                    <Col lg={11}>
+                        <FormControl as="textarea" rows="5" name="Message" ref={register({required: "Input required"})} className={styles.Input}/>
+                        <ErrorMessage name="Message" errors={errors} />
+                    </Col>
+                </Form.Row>
+
+                <Form.Row className={styles.Button}>
+                    <Buttons type="SubmitBtn"/>
+                </Form.Row>
+
+            </Form>
+        </div>
+    )
+}
 
 export default ContactForm;
